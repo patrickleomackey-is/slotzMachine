8e9be83c6b2f888e7ed371133469b4140a5a6e8c
patrickleomackey-is <plmackey@my.niacc.edu>
2020-12-02 20:14:14 -0600

    Slot machine

--- HTML5Application/public_html/slotMacJS.js
+++ HTML5Application/public_html/slotMacJS.js
@@ -5,14 +5,25 @@ BETs = document.getElementsByID('BET').onSubmit;
 const WHEEL =['Orange','Melon','Bar','Cherry','Bell','Plum'];
 
 
-spin();
+
 
 
 function spin()
 { 
     Spinner = Number(Math.floor(Math.random()*5));
-   return document.write(Spinner, WHEEL[Spinner]);
-   
+   if (Spinner === 0)
+       Spin = WHEEL[0];
+   if (Spinner === 1)
+       Spin = WHEEL[1];
+   if (Spinner === 2)
+       Spin = WHEEL[2];
+   if (Spinner === 3)
+       Spin = WHEEL[3];
+   if (Spinner === 4)
+       Spin = WHEEL[4];
+   if (Spinner === 5)
+       Spin = WHEEL[5];
+   return Spin;
 }
 
 
