49c5ef91bca6650cfa1ace5d90c407604a368eaf
patrickleomackey-is <plmackey@my.niacc.edu>
2020-12-02 19:53:08 -0600

    Slot machine

--- HTML5Application/public_html/slotMacJS.js
+++ HTML5Application/public_html/slotMacJS.js
@@ -1,4 +1,18 @@
 /* Patrick L. Mackey CIS 119 Prof. K. Miller
  */
+BETs = document.getElementsByID('BET').onSubmit;
+
+const WHEEL =['Orange','Melon','Bar','Cherry','Bell','Plum'];
+
+
+spin();
+
+
+function spin()
+{ 
+    Spinner = Number(Math.floor(Math.random()*5));
+   return document.write(Spinner, WHEEL[Spinner]);
+   
+}
 
 
