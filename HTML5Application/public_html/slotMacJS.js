ea87a79bae400f55a07965a5739c2bbfa01e0287
patrickleomackey-is <plmackey@my.niacc.edu>
2020-12-09 17:12:56 -0600

    n' theory works like a slot machine.  The input doesnt work 8( the pull function works 8) the spin function works 8) the compare and bet work 8) it works but not quite like I want it to

--- HTML5Application/public_html/slotMacJS.js
+++ HTML5Application/public_html/slotMacJS.js
@@ -4,22 +4,42 @@
 //T= document.getElementById(T);
 /*global globalMessage*/
  const WHEEL =['Orange','Melon','Bar','Cherry','Bell','Plum'];
+let winnings;
 
 
+
+
+
+function winns(){
+    document.write(window.content.localstorage[winnings]);
+}
+
+function getbet()
+{
+BET= Number(prompt("Enter a wager"));
+return BET;
+}
 function wheel()
 {
-BET = document.getElementById('B').innerHTML;
+
 wheel1=spin(1);
 wheel2=spin(2);
 wheel3=spin(3);
- if ((wheel1 === wheel2)&&(wheel1 === wheel3))
-     result = BET*3;
- if ((wheel1 === wheel2)||(wheel1 === wheel3)||(wheel2 === wheel3))
-   result = BET*2;
+
+
+
+if ((wheel1 === wheel2)&&(wheel2 === wheel3))
+    result = BET * 3;
+ else if ((wheel1 === wheel2)||(wheel1 === wheel3)||(wheel2 === wheel3))
+     result = BET * 2;
+else result = BET * 0;
 document.write("<br>" + wheel1);
 document.write(wheel2);
 document.write(wheel3);
+document.write("<br>"+result);
 
+
+return result;
 };
 
 
